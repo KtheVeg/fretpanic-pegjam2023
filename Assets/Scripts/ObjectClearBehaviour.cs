9d5980057a79f0b1338ac012a32e08b5ef5f02a1
KtheVeg <60745091+KtheVeg@users.noreply.github.com>
2023-02-26 13:49:32 -0600

    Added vfx and other cool stuff
    Clear effect is now C O L O U R E D

--- Assets/Scripts/ObjectClearBehaviour.cs
+++ Assets/Scripts/ObjectClearBehaviour.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class ObjectClearBehaviour : MonoBehaviour
 {
     float currentState = 0f;
+    public Color colour;
     public void ClearObjects(Vector3 pos)
     {
         currentState = 0f;
         transform.localScale = Vector3.zero;
         transform.position = pos;
+        gameObject.GetComponent<SpriteRenderer>().color = colour;
+    }
+    public void ClearObjects(Vector3 pos, Color col)
+    {
+        currentState = 0f;
+        transform.localScale = Vector3.zero;
+        transform.position = pos;
+        gameObject.GetComponent<SpriteRenderer>().color = col;
+        colour = col;
     }
 
     // Update is called once per frame
@@ -22,7 +32,8 @@ public class ObjectClearBehaviour : MonoBehaviour
             return;
         }
         currentState += Time.deltaTime;
-        transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(15,15,15), currentState);
+        transform.localScale = Vector3.Lerp(Vector3.zero, new Vector3(3,3,3), currentState);
+        gameObject.GetComponent<SpriteRenderer>().color = Color.Lerp(colour, new Color(0,0,0,0), currentState);
     }
 
     void OnTriggerEnter(Collider other)
--- Assets/Scripts/PlayerCollider.cs
+++ Assets/Scripts/PlayerCollider.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerCollider : MonoBehaviour
 {
+    public bool shielded;
     public GameObject clearObj;
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Damage")
+        switch (other.tag)
         {
-            // --DO DAMAGE OR SOMETHING -- //
-            
-
-            clearObj.GetComponent<ObjectClearBehaviour>().ClearObjects(transform.position);
+            case "Damage":
+                // --DO DAMAGE OR SOMETHING -- //
+                
+                
+                clearObj.GetComponent<ObjectClearBehaviour>().ClearObjects(transform.position, shielded ? new Color(255,0,0) : new Color(0,0,255));
+            break;
+            case "Powerup":
+                switch (other.gameObject.GetComponent<Powerup>().powerupType)
+                {
+                    case PowType.HP:
+                        // Health
+                    break;
+                    case PowType.Shield:
+                        // Shield
+                    break;
+                    case PowType.Clear:
+                        // Clear
+                        clearObj.GetComponent<ObjectClearBehaviour>().ClearObjects(transform.position, new Color(0,255,0));
+                    break;
+                }
+            break;
         }
     }
 }
